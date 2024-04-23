346f8f9d6d6024b0c394e04be2d295f38a87cb52
jlostao <jorlosnav@gmail.com>
2024-04-23 16:51:43 +0200

    modified:   src/config/db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URI: 'mongodb://root:passpj03@localhost:27017/wordQuestDB?authSource=admin',
-    MONGODB_URI_TEST: 'mongodb://root:passpj03@localhost:27017/wordQuestDB?authSource=admin' // Base de dades de test
+    MONGODB_URI: 'mongodb://cristian:1234@localhost:27017/wordQuestDB?authSource=admin',
+    MONGODB_URI_TEST: 'mongodb://cristian:1234@localhost:27017/wordQuestDB?authSource=admin' // Base de dades de test
 };
