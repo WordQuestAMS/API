ab4eb71351f372074f5b4e090f6e73a309d0d85a
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2024-04-30 17:47:12 +0200

    dsa

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const app = require('./app');
 const http = require('http');
 const { Server } = require('socket.io');
 
-const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
@@ -10,7 +9,7 @@ class Joc {
   constructor(partidaDuracio, pausaDuracio) {
     this.partidaDuracio = partidaDuracio;
     this.pausaDuracio = pausaDuracio;
-    this.properInici = Date.now() + this.pausaDuracio;  // Comienza con un periodo de pausa
+    this.properInici = Date.now() + this.partidaDuracio + this.pausaDuracio;
     this.enPartida = false;
     this.iniciarCicle();
   }
@@ -18,15 +17,11 @@ class Joc {
   iniciarCicle() {
     setInterval(() => {
       if (this.enPartida) {
-        console.log("La partida ha terminado, 1 minuto para la siguiente partida");
         this.properInici = Date.now() + this.pausaDuracio;
         this.enPartida = false;
-        io.emit('ESTAT_PARTIDA', { message: 'La partida ha terminado, 1 minuto para la siguiente partida' });
       } else {
-        console.log("Comienza la partida");
-        this.properInici = Date.now() + this.partidaDuracio;
+        this.properInici = Date.now() + this.partidaDuracio + this.pausaDuracio;
         this.enPartida = true;
-        io.emit('ESTAT_PARTIDA', { message: 'Comienza la partida' });
       }
     }, this.partidaDuracio + this.pausaDuracio);
   }
@@ -35,49 +30,36 @@ class Joc {
     const tempsRestant = this.properInici - Date.now();
     return { tempsRestant, enPartida: this.enPartida };
   }
-
-  esPotUnir() {
-    return !this.enPartida;
-  }
 }
 
-const joc = new Joc(60000, 60000);  // 1 minuto de juego, 1 minuto de pausa
+const joc = new Joc(60000, 60000);  // 1 minut de partida, 1 minut de pausa
 
 io.on('connection', (socket) => {
-  console.log('Usuario conectado');
+  console.log('Usuari connectat');
+  
+  const intervalId = setInterval(() => {
+    const resposta = joc.consultaTempsRestant();
+    socket.emit('TEMPS_PER_INICI', resposta);
+  }, 10000);  // Envia el temps restant cada 10 segons
 
   socket.on('TEMPS_PER_INICI', () => {
-    const respuesta = joc.consultaTempsRestant();
-    socket.emit('TEMPS_PER_INICI', respuesta);
+    const resposta = joc.consultaTempsRestant();
+    socket.emit('TEMPS_PER_INICI', resposta);
   });
 
-  socket.on('ALTA', (data) => {
-    if (joc.esPotUnir()) {
-      console.log(`Usuario unido a la partida: Nickname: ${data.nickname}, API_KEY: ${data.apiKey}`);
-      // Aquí puedes añadir el usuario a la lista de jugadores, etc.
-    } else {
-      console.log('Intento de unirse a una partida ya comenzada');
-      socket.emit('ERROR', 'La partida ya ha comenzado');
+  socket.onAny((event, ...args) => {
+    if (event !== 'consulta temps' && event !== 'disconnect' && event !== 'connect') {
+      console.log(`Comanda no reconeguda: ${event}`);
+      const resposta = joc.consultaTempsRestant();
+      socket.emit('TEMPS_PER_INICI', resposta);
     }
   });
 
-  // Maneja el evento 'PARAULA'
-  socket.on('PARAULA', (data) => {
-    // Deberías parsear la cadena para extraer correctamente la palabra y la API_KEY si es necesario
-    const params = data.split(';').reduce((acc, current) => {
-      const [key, value] = current.split('=');
-      acc[key] = value;
-      return acc;
-    }, {});
-
-    console.log(`Paraula rebuda: ${params.PALABRA}`);
-    // Añadir lógica de manejo de palabras si es necesario
-  });
-
   socket.on('disconnect', () => {
-    console.log('Usuario desconectado');
+    console.log('Usuari desconnectat');
+    clearInterval(intervalId);  // Atura l'enviament periòdic quan l'usuari es desconnecta
   });
 });
 
 const port = process.env.PORT || 80;
-server.listen(port, () => console.log(`Escuchando en el puerto ${port}...`));
+server.listen(port, () => console.log(`Escoltant en el port ${port}...`));
